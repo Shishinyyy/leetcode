dca5d240553a0995947dcdf67821ad7cb0dd7a6d
Shishinyyy <a.shishinyy@gmail.com>
2024-11-24 00:09:18 +0400

    [1323] [Time Beats: 100.00%] [Memory Beats: 48.35%] - LeetPush

---
+++ 1323-Maximum-69-Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximum69Number(int num) {
+        std::string str = to_string(num);
+        for (auto& x : str) {
+            if (x == '6') {
+                x = '9';
+                break;
+            }
+        }
+        return stoi(str);
+    }
+};
