b2efff012313eee2f8d44647b992bff7948d30e1
Shishinyyy <a.shishinyy@gmail.com>
2025-01-03 10:23:59 +0400

    [1929] [Time Beats: 100.00%] [Memory Beats: 39.65%] - LeetPush

--- 1929-Concatenation-of-Array.cpp
+++ 1929-Concatenation-of-Array.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
+        vector<int> ans(2 * n);
         for (int i = 0; i < n; i++) {
-            nums.push_back(nums[i]);
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
         }
-        return nums;
+        return ans;
     }
 };
