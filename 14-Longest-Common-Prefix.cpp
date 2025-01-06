bfdf7ed6d4ae73b4c5d98ea77bce378a3e6c0595
Shishinyyy <a.shishinyy@gmail.com>
2025-01-06 14:53:54 +0400

    [14] [Time Beats: 100.00%] [Memory Beats: 28.43%] - LeetPush

---
+++ 14-Longest-Common-Prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(), strs.end());
+        int n = strs[0].size();
+        string ans = \\;
+        for (int i = 0; i < n; i++) {
+            char l = strs[0][i];
+            for (int j = 1; j < strs.size(); j++) {
+                if (strs[j][i] != l)
+                    return ans;
+            }
+            ans += strs[0][i];
+        }
+        return ans;
+    }
+};
