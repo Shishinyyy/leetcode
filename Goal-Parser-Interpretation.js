91a32496ece3c2687399f7f41ed36938965a6044
Abdelrahman Elshishiny <a.shishinyy@gmail.com>
2025-03-23 12:11:11 +0400

    [1678] [Time Beats: 76.66%] [Memory Beats: 5.19%] - LeetPush

---
+++ Goal-Parser-Interpretation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function (command) {
+    let ans = "";
+    for (let i = 0; i < command.length; i++) {
+        if (command[i] === 'G') {
+            ans += 'G';
+        }
+        else if (command[i] === '(' && command[i + 1] === ')') {
+            ans += 'o';
+        }
+        else if (command[i] === 'a') {
+            ans += "al";
+        }
+    }
+    return ans;
+};
