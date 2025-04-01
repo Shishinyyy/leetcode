d82d857478a5d78501ba48e6f2f17ad63e946c48
Abdelrahman Elshishiny <a.shishinyy@gmail.com>
2025-04-02 01:27:52 +0400

    [3289] [Time Beats: 100.00%] [Memory Beats: 30.71%] - LeetPush

--- The-Two-Sneaky-Numbers-of-Digitville.cpp
+++ The-Two-Sneaky-Numbers-of-Digitville.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
+        unordered_map<int, int> freq;
         vector<int> ans;
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size() - 1; i++ ) {
-            if(nums[i] == nums[i+1])
-                ans.push_back(nums[i]);
+        for (auto num : nums) {
+            freq[num]++;
+            if (freq[num] > 1) {
+                ans.push_back(num);
+            }
         }
         return ans;
     }
