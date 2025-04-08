cf8690218b18bbbd24850051808cd93ac4d56b88
Abdelrahman Elshishiny <a.shishinyy@gmail.com>
2025-04-09 00:27:14 +0400

    [3065] [Time Beats: 100.00%] [Memory Beats: 80.87%] - LeetPush

--- Minimum-Operations-to-Exceed-Threshold-Value-I.cpp
+++ Minimum-Operations-to-Exceed-Threshold-Value-I.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int count = 0;
-        sort(nums.begin(), nums.end());
         for(auto num : nums) {
             if(num < k) {
                 count++;
